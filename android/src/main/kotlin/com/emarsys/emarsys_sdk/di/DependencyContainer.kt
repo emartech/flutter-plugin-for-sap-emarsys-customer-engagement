8520f5c02629e2b1974bf37536584cdeb6bd4b4c
dschuppa <d.schuppa@gmail.com>
2021-04-22 15:58:50 +0200

    feat(dependency-container): remove DependencyInjection.kt SUITEDEV-27523
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>

--- android/src/main/kotlin/com/emarsys/emarsys_sdk/di/DependencyContainer.kt
+++ android/src/main/kotlin/com/emarsys/emarsys_sdk/di/DependencyContainer.kt
@@ -2,7 +2,16 @@ package com.emarsys.emarsys_sdk.di
 
 import com.emarsys.emarsys_sdk.commands.EmarsysCommandFactory
 
-fun dependencyContainer() = DependencyContainer.instance ?: throw IllegalStateException("DependencyContainer has to be setup first!")
+fun dependencyContainer() = DependencyContainer.instance
+        ?: throw IllegalStateException("DependencyContainer has to be setup first!")
+
+fun tearDownDependencyContainer() { DependencyContainer.instance = null }
+
+fun setupDependencyContainer(container: DependencyContainer) {
+    if (DependencyContainer.instance == null) {
+        DependencyContainer.instance = container
+    }
+}
 
 interface DependencyContainer {
     companion object {
