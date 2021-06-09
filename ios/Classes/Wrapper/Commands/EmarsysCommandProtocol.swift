8f3d05070116d96389300b54bc4c8e372dfde451
Mihály Hunyady <mihaly.hunyady@emarsys.com>
2021-06-09 14:42:56 +0200

    refactor(packages): cleanup package structure SUITEDEV-28064
    Co-authored-by: davidSchuppa <32750715+davidSchuppa@users.noreply.github.com>
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>
    Co-authored-by: kovacszsoltizsolt <22084766+kovacszsoltizsolt@users.noreply.github.com>

--- ios/Classes/Wrapper/Commands/EmarsysCommandProtocol.swift
+++ ios/Classes/Wrapper/Commands/EmarsysCommandProtocol.swift
@@ -1,3 +1,5 @@
+typealias ResultCallback = (_ result: [String: Any]) -> ()
+
 protocol EmarsysCommandProtocol {
     
     func execute(arguments: [String: Any]?, resultCallback: @escaping ResultCallback)
