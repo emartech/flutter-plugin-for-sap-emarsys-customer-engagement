83c410ac609ee7a045dc4bea37cbcdc6c3284b9c
Andras Sarro <andras.sarro@emarsys.com>
2021-10-25 15:28:01 +0200

    feat(predict): finished recommendationLogicMapper SUITEDEV-28643
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>
    Co-authored-by: kovacszsoltizsolt <22084766+kovacszsoltizsolt@users.noreply.github.com>

---
+++ android/src/main/kotlin/com/emarsys/emarsys_sdk/mapper/RecommendationLogicMapper.kt
@@ -0,0 +1,81 @@
+package com.emarsys.emarsys_sdk.mapper
+
+import com.emarsys.core.Mapper
+import com.emarsys.predict.api.model.CartItem
+import com.emarsys.predict.api.model.Logic
+import com.emarsys.predict.api.model.PredictCartItem
+import com.emarsys.predict.api.model.RecommendationLogic
+import java.lang.IllegalArgumentException
+
+class RecommendationLogicMapper : Mapper<Map<String, Any?>, Logic> {
+
+    override fun map(input: Map<String, Any?>): Logic {
+        val logicName =
+            input["name"] as String? ?: throw IllegalArgumentException("Name should not be empty!")
+        val data = input["data"] as Map<String, Any>?
+            ?: throw IllegalArgumentException("Data should not be empty!")
+        val variants = input["variants"] as List<String>?
+
+        return when (logicName) {
+            RecommendationLogic.SEARCH ->
+                if (data["searchTerm"] != null) {
+                    RecommendationLogic.search(data["searchTerm"] as String)
+                } else {
+                    RecommendationLogic.search()
+                }
+            RecommendationLogic.CART ->
+                if (data["items"] != null) {
+                    RecommendationLogic.cart(mapCartItems(data["items"] as List<Map<String, Any>>))
+                } else {
+                    RecommendationLogic.cart()
+                }
+            RecommendationLogic.CATEGORY ->
+                if (data["categoryPath"] != null) {
+                    RecommendationLogic.category(data["categoryPath"] as String)
+                } else {
+                    RecommendationLogic.category()
+                }
+            RecommendationLogic.RELATED ->
+                if (data["itemId"] != null) {
+                    RecommendationLogic.related(data["itemId"] as String)
+                } else {
+                    RecommendationLogic.related()
+                }
+            RecommendationLogic.ALSO_BOUGHT ->
+                if (data["itemId"] != null) {
+                    RecommendationLogic.alsoBought(data["itemId"] as String)
+                } else {
+                    RecommendationLogic.alsoBought()
+                }
+            RecommendationLogic.POPULAR ->
+                if (data["categoryPath"] != null) {
+                    RecommendationLogic.popular(data["categoryPath"] as String)
+                } else {
+                    RecommendationLogic.popular()
+                }
+            RecommendationLogic.PERSONAL ->
+                if (variants != null) {
+                    RecommendationLogic.personal(variants)
+                } else {
+                    RecommendationLogic.personal(listOf())
+                }
+            RecommendationLogic.HOME ->
+                if (variants != null) {
+                    RecommendationLogic.home(variants)
+                } else {
+                    RecommendationLogic.home(listOf())
+                }
+            else -> throw IllegalArgumentException("Invalid logic name!")
+        }
+    }
+
+    private fun mapCartItems(cartItems: List<Map<String, Any>>): List<CartItem> {
+        return cartItems.map {
+            PredictCartItem(
+                it["itemId"] as String,
+                (it["price"] as Int).toDouble(),
+                (it["quantity"] as Int).toDouble()
+            )
+        }.toList()
+    }
+}
---
+++ android/src/test/kotlin/com/emarsys/emarsys_sdk/mapper/RecommendationLogicMapperTest.kt
@@ -0,0 +1,301 @@
+package com.emarsys.emarsys_sdk.mapper
+
+import arrow.core.mapOf
+import com.emarsys.predict.api.model.CartItem
+import com.emarsys.predict.api.model.Logic
+import com.emarsys.predict.api.model.PredictCartItem
+import com.emarsys.predict.api.model.RecommendationLogic
+import io.kotlintest.shouldBe
+import io.mockk.*
+import org.junit.After
+
+import org.junit.Before
+import org.junit.Test
+
+class RecommendationLogicMapperTest {
+    private lateinit var mapper: RecommendationLogicMapper
+    private lateinit var mockLogic: Logic
+
+    @Before
+    fun setUp() {
+        mockkStatic("com.emarsys.predict.api.model.RecommendationLogic")
+        mockLogic = mockk()
+        mapper = RecommendationLogicMapper()
+    }
+
+    @After
+    fun tearDown() {
+        unmockkStatic(RecommendationLogic::class)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun test_mapShouldThrowException() {
+        mapper.map(mapOf())
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun test_mapShouldThrowException_whenNameIsMissing() {
+        mapper.map(
+            mapOf(
+                "data" to emptyMap<String, Any>(),
+                "variants" to emptyList<String>()
+            )
+        )
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun test_mapShouldThrowException_whenDataIsMissing() {
+        mapper.map(
+            mapOf(
+                "name" to "testName",
+                "variants" to emptyList<String>()
+            )
+        )
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun test_mapShouldThrowException_whenVariantsIsMissing() {
+        mapper.map(
+            mapOf(
+                "name" to "testName",
+                "data" to emptyMap<String, Any>(),
+            )
+        )
+    }
+
+    @Test
+    fun testMap_shouldMapSearchLogic() {
+        val result = mapper.map(
+            mapOf(
+                "name" to RecommendationLogic.SEARCH,
+                "data" to mapOf("searchTerm" to "testSearchTerm"),
+                "variants" to emptyList<String>()
+            )
+        )
+
+        result.logicName shouldBe RecommendationLogic.SEARCH
+        result.data shouldBe mapOf("q" to "testSearchTerm")
+    }
+
+    @Test
+    fun testMap_shouldMapSearchLogic_withoutSearchTerm() {
+        val result = mapper.map(
+            mapOf(
+                "name" to RecommendationLogic.SEARCH,
+                "data" to mapOf<String, Any>(),
+                "variants" to emptyList<String>()
+            )
+        )
+
+        result.logicName shouldBe RecommendationLogic.SEARCH
+        result.data shouldBe mapOf()
+    }
+
+    @Test
+    fun testMap_shouldMapCartLogic() {
+        val result = mapper.map(
+            mapOf(
+                "name" to RecommendationLogic.CART,
+                "data" to mapOf<String, Any>(),
+                "variants" to emptyList<String>()
+            )
+        )
+
+        result.logicName shouldBe RecommendationLogic.CART
+        result.data shouldBe mapOf()
+    }
+
+    @Test
+    fun testMap_shouldMapCartLogic_WithItems() {
+        val result = mapper.map(
+            mapOf(
+                "name" to RecommendationLogic.CART,
+                "data" to mapOf<String, Any>(
+                    "items" to listOf(
+                        mapOf(
+                            "itemId" to "testItemId1",
+                            "price" to 123,
+                            "quantity" to 234
+                        ), mapOf(
+                            "itemId" to "testItemId2",
+                            "price" to 456,
+                            "quantity" to 567
+                        )
+                    )
+                ),
+                "variants" to emptyList<String>()
+            )
+        )
+
+        result.logicName shouldBe RecommendationLogic.CART
+        result.data.containsKey("ca") shouldBe true
+    }
+
+    @Test
+    fun testMap_shouldMapSearchCategory() {
+        val result = mapper.map(
+            mapOf(
+                "name" to RecommendationLogic.CATEGORY,
+                "data" to mapOf("categoryPath" to "testCategoryPath"),
+                "variants" to emptyList<String>()
+            )
+        )
+
+        result.logicName shouldBe RecommendationLogic.CATEGORY
+        result.data shouldBe mapOf("vc" to "testCategoryPath")
+    }
+
+    @Test
+    fun testMap_shouldMapCategoryLogic_withoutCategoryPath() {
+        val result = mapper.map(
+            mapOf(
+                "name" to RecommendationLogic.CATEGORY,
+                "data" to mapOf<String, Any>(),
+                "variants" to emptyList<String>()
+            )
+        )
+
+        result.logicName shouldBe RecommendationLogic.CATEGORY
+        result.data shouldBe mapOf()
+    }
+
+    @Test
+    fun testMap_shouldMapRelatedLogic_withRelatedItemId() {
+        val result = mapper.map(
+            mapOf(
+                "name" to RecommendationLogic.RELATED,
+                "data" to mapOf("itemId" to "testItemId"),
+                "variants" to emptyList<String>()
+            )
+        )
+
+        result.logicName shouldBe RecommendationLogic.RELATED
+        result.data shouldBe mapOf("v" to "i:testItemId")
+    }
+
+    @Test
+    fun testMap_shouldMapRelatedLogic_withoutRelatedItemId() {
+        val result = mapper.map(
+            mapOf(
+                "name" to RecommendationLogic.RELATED,
+                "data" to mapOf<String, Any>(),
+                "variants" to emptyList<String>()
+            )
+        )
+
+        result.logicName shouldBe RecommendationLogic.RELATED
+        result.data shouldBe mapOf()
+    }
+
+    @Test
+    fun testMap_shouldMapAlsoBoughtLogic_withRelatedItemId() {
+        val result = mapper.map(
+            mapOf(
+                "name" to RecommendationLogic.ALSO_BOUGHT,
+                "data" to mapOf("itemId" to "testItemId"),
+                "variants" to emptyList<String>()
+            )
+        )
+
+        result.logicName shouldBe RecommendationLogic.ALSO_BOUGHT
+        result.data shouldBe mapOf("v" to "i:testItemId")
+    }
+
+    @Test
+    fun testMap_shouldMapAlsoBoughtLogic_withoutRelatedItemId() {
+        val result = mapper.map(
+            mapOf(
+                "name" to RecommendationLogic.ALSO_BOUGHT,
+                "data" to mapOf<String, Any>(),
+                "variants" to emptyList<String>()
+            )
+        )
+
+        result.logicName shouldBe RecommendationLogic.ALSO_BOUGHT
+        result.data shouldBe mapOf()
+    }
+
+    @Test
+    fun testMap_shouldMapPopularLogic_withCategoryPath() {
+        val result = mapper.map(
+            mapOf(
+                "name" to RecommendationLogic.POPULAR,
+                "data" to mapOf("categoryPath" to "testCategoryPath"),
+                "variants" to emptyList<String>()
+            )
+        )
+
+        result.logicName shouldBe RecommendationLogic.POPULAR
+        result.data shouldBe mapOf("vc" to "testCategoryPath")
+    }
+
+    @Test
+    fun testMap_shouldMapPopularLogic_withoutCategoryPath() {
+        val result = mapper.map(
+            mapOf(
+                "name" to RecommendationLogic.POPULAR,
+                "data" to mapOf<String, Any>(),
+                "variants" to emptyList<String>()
+            )
+        )
+
+        result.logicName shouldBe RecommendationLogic.POPULAR
+        result.data shouldBe mapOf()
+    }
+
+    @Test
+    fun testMap_shouldMapPersonalLogic_withVariants() {
+        val result = mapper.map(
+            mapOf(
+                "name" to RecommendationLogic.PERSONAL,
+                "data" to mapOf<String, Any>(),
+                "variants" to listOf("testVariant1", "testVariant2")
+            )
+        )
+
+        result.logicName shouldBe RecommendationLogic.PERSONAL
+        result.variants shouldBe listOf("testVariant1", "testVariant2")
+    }
+
+    @Test
+    fun testMap_shouldMapPersonalLogic_withNullAsVariants() {
+        val result = mapper.map(
+            mapOf(
+                "name" to RecommendationLogic.PERSONAL,
+                "data" to mapOf<String, Any>(),
+                "variants" to null
+            )
+        )
+
+        result.logicName shouldBe RecommendationLogic.PERSONAL
+        result.variants shouldBe emptyList()
+    }
+
+    @Test
+    fun testMap_shouldMapHomeLogic_withVariants() {
+        val result = mapper.map(
+            mapOf(
+                "name" to RecommendationLogic.HOME,
+                "data" to mapOf<String, Any>(),
+                "variants" to listOf("testVariant1", "testVariant2")
+            )
+        )
+
+        result.logicName shouldBe RecommendationLogic.HOME
+        result.variants shouldBe listOf("testVariant1", "testVariant2")
+    }
+
+    @Test
+    fun testMap_shouldMapHomeLogic_withNullAsVariants() {
+        val result = mapper.map(
+            mapOf(
+                "name" to RecommendationLogic.HOME,
+                "data" to mapOf<String, Any>(),
+                "variants" to null
+            )
+        )
+
+        result.logicName shouldBe RecommendationLogic.HOME
+        result.variants shouldBe emptyList()
+    }
+}
