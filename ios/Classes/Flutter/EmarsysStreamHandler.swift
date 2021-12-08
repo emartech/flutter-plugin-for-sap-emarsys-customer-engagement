a91fa2ae74face101047ef3186067b8ae67f9b56
Hunyady Mihály <mihaly.hunyady@emarsys.com>
2021-12-08 15:31:42 +0100

    feat(event-handler): add cache to eventHandlers SUITEDEV-29700
    Co-authored-by: LordAndras <49073629+LordAndras@users.noreply.github.com>
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>

--- ios/Classes/Flutter/EmarsysStreamHandler.swift
+++ ios/Classes/Flutter/EmarsysStreamHandler.swift
@@ -6,19 +6,24 @@ import Foundation
 import EmarsysSDK
 
 class EmarsysStreamHandler: NSObject, FlutterStreamHandler {
-    
+
     var sink: FlutterEventSink?
     var eventHandler: EMSEventHandlerBlock?
     var voidHandler: (() -> ())!
     var completionHandler: ((Error?) -> ())!
-    
+    var cache: [(String, [String: Any])] = []
+
     override init() {
         super.init()
         self.eventHandler = { [unowned self] eventName, payload in
-            var event = [String: Any]()
-            event["name"] = eventName
-            event["payload"] = payload
-            self.sink?(event)
+            if (sink == nil) {
+                cache.append((eventName, payload ?? [:]))
+            } else {
+                var event = [String: Any]()
+                event["name"] = eventName
+                event["payload"] = payload
+                self.sink?(event)
+            }
         }
         self.voidHandler = {
             self.sink?(nil)
@@ -27,12 +32,19 @@ class EmarsysStreamHandler: NSObject, FlutterStreamHandler {
             self.sink?(error)
         }
     }
-        
+
     func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
         sink = events
+        cache.forEach { cachedEvent in
+            var event: [String: Any] = [:]
+            event["name"] = cachedEvent.0
+            event["payload"] = cachedEvent.1
+            self.sink?(event)
+        }
+        cache.removeAll()
         return nil
     }
-    
+
     func onCancel(withArguments arguments: Any?) -> FlutterError? {
         return nil
     }
