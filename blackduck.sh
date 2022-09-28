c2dbe524210865b0bfb50287cd9b5475377ab820
Hunyady Mihály <mihaly.hunyady@emarsys.com>
2022-09-28 15:01:12 +0200

    chore(blackduck): remove deprecated parameters SUITEDEV-31868
    Co-authored-by: LordAndras <49073629+LordAndras@users.noreply.github.com>

--- blackduck.sh
+++ blackduck.sh
@@ -1,14 +1,13 @@
 curl --silent -O https://detect.synopsys.com/detect.sh
 chmod +x detect.sh
 ./detect.sh \
---blackduck.timeout=6000 \
+--detect.timeout=6000 \
 --blackduck.trust.cert=true \
 --detect.blackduck.signature.scanner.memory=4096 \
 --detect.blackduck.signature.scanner.paths=~/work/flutter-plugin-for-sap-emarsys-customer-engagement \
 --detect.code.location.name="https://github.com/emartech/flutter-plugin-for-sap-emarsys-customer-engagement" \
 --detect.project.name="Github:emartech\ /\ flutter-plugin-for-sap-emarsys-customer-engagement" \
 --detect.project.version.name=master \
---detect.report.timeout=4800 \
 --detect.source.path=~/work/flutter-plugin-for-sap-emarsys-customer-engagement \
 --detect.npm.include.dev.dependencies=false \
 --detect.sbt.report.search.depth=4 \
