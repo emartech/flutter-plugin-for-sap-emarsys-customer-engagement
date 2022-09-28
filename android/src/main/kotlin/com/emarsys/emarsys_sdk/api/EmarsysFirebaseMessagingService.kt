aa4cab08db17690df6bc693b45adebdb2cacb495
davidSchuppa <d.schuppa@gmail.com>
2022-09-28 10:10:32 +0200

    fix(push): notifications did not arrive when app was killed SUITEDEV-31868
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>
    Co-authored-by: Andras Sarro <lordandras@users.noreply.github.com>
    Co-authored-by: fbokos <99868374+fbokor@users.noreply.github.com>

--- android/src/main/kotlin/com/emarsys/emarsys_sdk/api/EmarsysFirebaseMessagingService.kt
+++ android/src/main/kotlin/com/emarsys/emarsys_sdk/api/EmarsysFirebaseMessagingService.kt
@@ -1,6 +1,11 @@
 package com.emarsys.emarsys_sdk.api
 
+import android.app.Application
 import android.content.Context
+import com.emarsys.Emarsys
+import com.emarsys.common.feature.InnerFeature
+import com.emarsys.config.ConfigLoader
+import com.emarsys.core.feature.FeatureRegistry
 import com.emarsys.di.isEmarsysComponentSetup
 import com.emarsys.emarsys_sdk.di.DefaultDependencyContainer
 import com.emarsys.emarsys_sdk.di.dependencyContainer
@@ -8,6 +13,8 @@ import com.emarsys.emarsys_sdk.di.dependencyContainerIsSetup
 import com.emarsys.emarsys_sdk.di.setupDependencyContainer
 import com.emarsys.emarsys_sdk.flutter.FlutterBackgroundExecutor
 import com.emarsys.emarsys_sdk.provider.MainHandlerProvider
+import com.emarsys.mobileengage.notification.NotificationCommandFactory
+import com.emarsys.mobileengage.service.NotificationActionUtils
 import com.emarsys.service.EmarsysFirebaseMessagingServiceUtils
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
@@ -15,52 +22,65 @@ import java.util.*
 
 class EmarsysFirebaseMessagingService : FirebaseMessagingService() {
     companion object {
+        private val configLoader = ConfigLoader()
+
         private val messageQueue: MutableList<RemoteMessage> =
-                Collections.synchronizedList(LinkedList())
+            Collections.synchronizedList(LinkedList())
 
         fun showInitialMessages(context: Context) {
             if (isEmarsysComponentSetup()) {
-                synchronized(messageQueue) {
-                    messageQueue.forEach {
-                        EmarsysFirebaseMessagingServiceUtils.handleMessage(context, it)
-                    }
-                    messageQueue.clear()
-                }
-            }
-        }
-    }
-
-    override fun onMessageReceived(message: RemoteMessage) {
-        super.onMessageReceived(message)
-        if (!isEmarsysComponentSetup()) {
-            initFlutterEngine {
-                MainHandlerProvider.provide().post {
-                    it.awakeFlutterCallback(dependencyContainer().flutterWrapperSharedPreferences)
-                }
-            }
-        }
-        synchronized(messageQueue) {
-            messageQueue.add(message)
-        }
-        showInitialMessages(this)
-    }
-
-    override fun onNewToken(pushToken: String) {
-        super.onNewToken(pushToken)
-        initFlutterEngine {
-            dependencyContainer().pushTokenStorage.pushToken = pushToken
-        }
-    }
-
-    private fun initFlutterEngine(onCompleted: (FlutterBackgroundExecutor) -> Unit) {
-        val executor = FlutterBackgroundExecutor(application)
-        executor.startBackgroundIsolate { dartExecutor ->
-            if (dependencyContainerIsSetup()) {
-                dependencyContainer().messenger = dartExecutor
+                handleMessageQueue(context)
             } else {
-                setupDependencyContainer(DefaultDependencyContainer(application, dartExecutor))
+                Emarsys.setup(
+                    configLoader.loadConfigFromSharedPref(
+                        context.applicationContext as Application,
+                        "emarsys_setup_cache"
+                    ).build()
+                )
+                handleMessageQueue(context)
             }
-            onCompleted.invoke(executor)
+        }
+
+        private fun handleMessageQueue(context: Context) {
+            synchronized(messageQueue) {
+                messageQueue.forEach {
+                    EmarsysFirebaseMessagingServiceUtils.handleMessage(context, it)
+                }
+                messageQueue.clear()
+            }
+        }
+
+        override fun onMessageReceived(message: RemoteMessage) {
+            super.onMessageReceived(message)
+            if (!isEmarsysComponentSetup()) {
+                initFlutterEngine {
+                    MainHandlerProvider.provide().post {
+                        it.awakeFlutterCallback(dependencyContainer().flutterWrapperSharedPreferences)
+                    }
+                }
+            }
+            synchronized(messageQueue) {
+                messageQueue.add(message)
+            }
+            showInitialMessages(this)
+        }
+
+        override fun onNewToken(pushToken: String) {
+            super.onNewToken(pushToken)
+            initFlutterEngine {
+                dependencyContainer().pushTokenStorage.pushToken = pushToken
+            }
+        }
+
+        private fun initFlutterEngine(onCompleted: (FlutterBackgroundExecutor) -> Unit) {
+            val executor = FlutterBackgroundExecutor(application)
+            executor.startBackgroundIsolate { dartExecutor ->
+                if (dependencyContainerIsSetup()) {
+                    dependencyContainer().messenger = dartExecutor
+                } else {
+                    setupDependencyContainer(DefaultDependencyContainer(application, dartExecutor))
+                }
+                onCompleted.invoke(executor)
+            }
         }
     }
-}
