4d4e42edb53204966d5a2f826542192986cfe988
LordAndras <sarrotanba@gmail.com>
2024-01-17 10:40:42 +0100

    fix(notification-service): fix notifications SUITEDEV-34999
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>

--- example/ios/NotificationExtension/NotificationService.swift
+++ example/ios/NotificationExtension/NotificationService.swift
@@ -6,30 +6,15 @@
 //
 
 import UserNotifications
+import EmarsysNotificationService
 
-class NotificationService: UNNotificationServiceExtension {
+class NotificationService: EMSNotificationService {
 
-    var contentHandler: ((UNNotificationContent) -> Void)?
-    var bestAttemptContent: UNMutableNotificationContent?
-
-    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
-        self.contentHandler = contentHandler
-        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-        
-        if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-            
-            contentHandler(bestAttemptContent)
-        }
-    }
-    
-    override func serviceExtensionTimeWillExpire() {
-        // Called just before the extension will be terminated by the system.
-        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
-            contentHandler(bestAttemptContent)
-        }
-    }
-
+    open override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+          if request.content.userInfo.contains(where: {$0.key as! String == "customLogicKey"}) {
+              // do smth wih your other notificationservice logic
+          } else {
+              super.didReceive(request, withContentHandler: contentHandler)
+          }
+      }
 }
